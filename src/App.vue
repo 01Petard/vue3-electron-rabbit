01e16ad469341b6830d9d2945a63778b56e8f6ce
01Petard <1520394133@qq.com>
2024-11-11 20:35:45 +0800

    feat: 1、学习了组件间的通信； 2、学习双向绑定、vue基础函数

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,145 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref, watch} from 'vue'
+import MyButton from './components/button.vue'
+
+const str = ref('')
+
+const list = ref([
+	{
+		isCompleted: false,
+		text: "吃饭"
+	},
+	{
+		isCompleted: false,
+		text: "睡觉"
+	},
+	{
+		isCompleted: false,
+		text: "打豆豆"
+	},
+])
+
+function add() {
+	if (str.value === '') {
+		alert('请输入内容')
+	} else {
+		list.value.push({
+			isCompleted: false,
+			text: str.value
+		})
+		str.value = ''
+	}
+}
+
+function del(index) {
+	list.value.splice(index, 1)
+}
+
+watch(list, (newList, oldList) => {
+	console.log('新的列表:', newList)
+	console.log('旧的列表:', oldList)
+}, {deep: true})
+
+function loadInfo(info) {
+	str.value = info
+	add()
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+	<div class="todo-app">
 
-  <main>
-    <TheWelcome />
-  </main>
+		<div class="title">Todo App</div>
+		<MyButton @sendInfo="loadInfo" text="你好" code="200"></MyButton>
+		<MyButton @sendInfo="loadInfo" text="我好" code="300"></MyButton>
+		<MyButton @sendInfo="loadInfo" text="大家好" code="400"></MyButton>
+		<div class="todo-form">
+			<input type="text" v-model="str" class="todo-input" placeholder="Add Todo...">
+			<div @click="add" class="todo-button">Add</div>
+		</div>
+
+		<div v-for="(item, index) in list" :key="index" class="item">
+			<div>
+				<input type="checkbox"/>
+				<span class="name">{{ index }}. {{ item.text }}</span>
+			</div>
+			<div @click="del(index)" class="del">del</div>
+		</div>
+
+	</div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.todo-app {
+	width: 300px;
+	margin: 50px auto;
+	padding: 20px;
+	background-color: #fff;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	border-radius: 8px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.title {
+	font-size: 24px;
+	font-weight: bold;
+	text-align: center;
+	margin-bottom: 20px;
+	color: #333;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.todo-form {
+	display: flex;
+	align-items: center;
+	margin-bottom: 20px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.todo-input {
+	flex-grow: 1;
+	padding: 10px;
+	border: 1px solid #ccc;
+	border-radius: 4px;
+	margin-right: 10px;
+	font-size: 16px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.todo-button {
+	cursor: pointer;
+	padding: 10px 20px;
+	background-color: #007bff;
+	color: #fff;
+	border: none;
+	border-radius: 4px;
+	font-size: 16px;
+	transition: background-color 0.3s;
+}
+
+.todo-button:hover {
+	background-color: #0056b3;
+}
+
+.item {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 10px 0;
+	border-bottom: 1px solid #eee;
+}
+
+.name {
+	font-size: 16px;
+	color: #333;
+}
+
+.del {
+	cursor: pointer;
+	color: #ff5252;
+	font-size: 16px;
+	transition: color 0.3s;
+}
+
+.del:hover {
+	color: #e51c23;
 }
 </style>
---
+++ src/components/button.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {defineEmits, defineProps} from 'vue'
+
+const props = defineProps([
+	'text',
+	'code'
+])
+
+const emit = defineEmits([
+	'sendInfo'
+])
+
+function sendInfo() {
+	emit('sendInfo', props.text + props.code)
+}
+
+</script>
+
+<template>
+	<div @click="sendInfo" class="button">{{ props.text }} : {{ props.code }}</div>
+</template>
+
+<style scoped>
+.button {
+	width: 100px;
+	height: 50px;
+	line-height: 50px;
+	text-align: center;
+	background-color: antiquewhite;
+	border-radius: 10px;
+}
+</style>
